96433d46fda59550294310716a66c25d47662b18
Huyh Huyhu <2509roblox@gmail.com>
2024-02-21 06:53:14 +0700

    first commit

---
+++ assets/js/menu.js
@@ -0,0 +1,10 @@
+var showDropMenu = function (name) {
+
+    var dropMenu = document.getElementsByClassName(name)[0];
+    dropMenu.style.display = "block";
+};
+
+var hideDropMenu = function (name) {
+    var dropMenu = document.getElementsByClassName(name)[0];
+    dropMenu.style.display = "none";
+};
