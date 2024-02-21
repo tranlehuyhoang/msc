685e312d6a5c47360b0ea3d71f5b23d540a074f8
Huyh Huyhu <2509roblox@gmail.com>
2024-02-21 08:47:36 +0700

    success

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,10 +1,34 @@
-var showDropMenu = function (name) {
+var showDropMenu = function (name, article) {
+    console.log('Left offset position:', article.offsetLeft, 'px');
 
     var dropMenu = document.getElementsByClassName(name)[0];
-    dropMenu.style.display = "block";
+    dropMenu.style.setProperty('display', 'block');
+    dropMenu.style.paddingLeft = article.offsetLeft + 'px';
 };
 
 var hideDropMenu = function (name) {
+    var elementsToHide = document.querySelectorAll(`.${name}`);
+    elementsToHide.forEach(function (element) {
+        element.style.display = "none";
+    });
     var dropMenu = document.getElementsByClassName(name)[0];
-    dropMenu.style.display = "none";
+    dropMenu.style.setProperty('display', 'none');
 };
+
+var articles = document.querySelectorAll('.menu-lTxfya article');
+
+articles.forEach(function (article) {
+    article.addEventListener('mouseover', function () {
+        this.classList.add('hover');
+        this.classList.remove('hover_');
+    });
+
+    article.addEventListener('mouseout', function () {
+        this.classList.add('hover_');
+        this.classList.remove('hover');
+
+        setTimeout(function () {
+            article.classList.remove('hover_');
+        }, 500);
+    });
+});
