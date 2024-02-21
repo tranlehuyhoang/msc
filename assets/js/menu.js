5235898bf6f05ff5dd276a97fab5f3a203c4cc10
Huyh Huyhu <2509roblox@gmail.com>
2024-02-21 20:32:02 +0700

    success mobile menu

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -48,11 +48,45 @@ document.querySelector('.navigation').addEventListener('click', function () {
     if (headerMobile.style.height === '100vh') {
         headerMobile.style.height = 'auto';
         document.querySelector('#header_mobile  #content').style.display = 'none';
+        document.querySelector('.frame-1000005533-pVIns9').style.opacity = '1';
+
         document.querySelector('.navigation').src = 'https://cdn.animaapp.com/projects/65d5e3adb758944670ebfa1e/releases/65d5e43b6f7b7e704bcb5c36/img/navigation-1.svg';
     } else {
         headerMobile.style.height = '100vh';
         document.querySelector('#header_mobile  #content').style.display = 'block';
+        document.querySelector('.frame-1000005533-pVIns9').style.opacity = '0';
 
         document.querySelector('.navigation').src = 'https://cdn.animaapp.com/projects/65d5e3adb758944670ebfa1e/releases/65d5e43b6f7b7e704bcb5c36/img/navigation.svg';
     }
 });
+
+// Lấy danh sách tất cả các phần tử <div> có lớp "component-187-dU3u59"
+var divs = document.getElementsByClassName("component-187-dU3u59");
+
+// Lặp qua từng phần tử <div> và thêm sự kiện nhấp chuột
+for (var i = 0; i < divs.length; i++) {
+    divs[i].addEventListener("click", function () {
+        console.log('first')
+        // Tìm phần tử con có lớp "frame-1000005551"
+        var frame = this.querySelector(".frame-1000005551");
+
+        // Lặp qua tất cả các phần tử có class ".frame-1000005551"
+        var allFrames = document.getElementsByClassName("frame-1000005551");
+        for (var j = 0; j < allFrames.length; j++) {
+            // Kiểm tra xem phần tử đó có là con của phần tử được nhấp chuột hay không
+            if (!this.contains(allFrames[j])) {
+                // Nếu không phải là con, ẩn đi
+                allFrames[j].style.display = "none";
+            }
+        }
+
+        // Kiểm tra trạng thái hiện tại của phần tử con
+        if (frame.style.display === "block") {
+            // Nếu đang hiển thị, ẩn đi
+            frame.style.display = "none";
+        } else {
+            // Nếu đang ẩn, hiển thị lên
+            frame.style.display = "block";
+        }
+    });
+}
