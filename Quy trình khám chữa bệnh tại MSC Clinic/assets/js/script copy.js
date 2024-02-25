cdd19497069f777c61c9b1378a1eb4e26e21f94d
Huyh Huyhu <2509roblox@gmail.com>
2024-02-25 09:02:26 +0700

    success hover img

---
+++ Quy trình khám chữa bệnh tại MSC Clinic/assets/js/script copy.js
@@ -0,0 +1,82 @@
+
+
+images_table[0].addEventListener("mouseover", function () {
+    images_table[0].src = "./assets/imgs/image 1.png";
+});
+
+images_table[1].addEventListener("mouseover", function () {
+    images_table[1].src = "./assets/imgs/image 1.png";
+});
+
+images_table[2].addEventListener("mouseover", function () {
+    images_table[2].src = "./assets/imgs/image 1.png";
+});
+
+images_table[6].addEventListener("mouseover", function () {
+    images_table[6].src = "./assets/imgs/image 1.png";
+});
+
+images_table[5].addEventListener("mouseover", function () {
+    images_table[5].src = "./assets/imgs/image 1.png";
+});
+
+images_table[4].addEventListener("mouseover", function () {
+    images_table[4].src = "./assets/imgs/image 1.png";
+});
+
+images_table[3].addEventListener("mouseover", function () {
+    images_table[3].src = "./assets/imgs/image 1.png";
+});
+
+images_table[7].addEventListener("mouseover", function () {
+    images_table[7].src = "./assets/imgs/image 1.png";
+});
+
+images_table[8].addEventListener("mouseover", function () {
+    images_table[8].src = "./assets/imgs/image 1.png";
+});
+
+images_table[9].addEventListener("mouseover", function () {
+    images_table[9].src = "./assets/imgs/image 1.png";
+});
+
+images_table[0].addEventListener("mouseout", function () {
+    images_table[0].src = "./assets/imgs/mask-group@2x.png";
+});
+
+images_table[1].addEventListener("mouseout", function () {
+    images_table[1].src = "./assets/imgs/mask-group-1@2x.png";
+});
+
+images_table[2].addEventListener("mouseout", function () {
+    images_table[2].src = "./assets/imgs/mask-group-5@2x.png";
+});
+
+images_table[6].addEventListener("mouseout", function () {
+    images_table[6].src = "./assets/imgs/mask-group-8@2x.png";
+});
+
+images_table[5].addEventListener("mouseout", function () {
+    images_table[5].src = "./assets/imgs/mask-group-10@2x.png";
+});
+
+images_table[4].addEventListener("mouseout", function () {
+    images_table[4].src = "./assets/imgs/Frame 1000007304 (1).png";
+});
+
+images_table[3].addEventListener("mouseout", function () {
+    images_table[3].src = "./assets/imgs/mask-group-15@2x.png";
+});
+
+images_table[7].addEventListener("mouseout", function () {
+    images_table[7].src = "./assets/imgs/mask-group-17@2x.png";
+});
+
+images_table[8].addEventListener("mouseout", function () {
+    images_table[8].src = "./assets/imgs/Frame 1000007305.svg";
+});
+
+images_table[9].addEventListener("mouseout", function () {
+    images_table[9].src = "./assets/imgs/mask-group-18@2x.png";
+});
+
---
+++ Quy trình khám chữa bệnh tại MSC Clinic/assets/js/script.js
@@ -0,0 +1,251 @@
+var images = document.querySelectorAll(".mask-group-BhxBYH.mask-group");
+var images_table = document.querySelectorAll(".container_table .mask-group-jbHgom.mask-group");
+var images_desktop = document.querySelectorAll(".container .mask-group-jbHgom.mask-group");
+
+images[0].addEventListener("mouseover", function () {
+    images[0].src = "./assets/imgs/image 1.png";
+});
+
+images[1].addEventListener("mouseover", function () {
+    images[1].src = "./assets/imgs/image 1.png";
+});
+
+images[2].addEventListener("mouseover", function () {
+    images[2].src = "./assets/imgs/image 1.png";
+});
+
+images[6].addEventListener("mouseover", function () {
+    images[6].src = "./assets/imgs/image 1.png";
+});
+
+images[5].addEventListener("mouseover", function () {
+    images[5].src = "./assets/imgs/image 1.png";
+});
+
+images[4].addEventListener("mouseover", function () {
+    images[4].src = "./assets/imgs/image 1.png";
+});
+
+images[3].addEventListener("mouseover", function () {
+    images[3].src = "./assets/imgs/image 1.png";
+});
+
+images[7].addEventListener("mouseover", function () {
+    images[7].src = "./assets/imgs/image 1.png";
+});
+
+images[8].addEventListener("mouseover", function () {
+    images[8].src = "./assets/imgs/image 1.png";
+});
+
+images[9].addEventListener("mouseover", function () {
+    images[9].src = "./assets/imgs/image 1.png";
+});
+
+images[0].addEventListener("mouseout", function () {
+    images[0].src = "./assets/imgs/mask-group@2x.png";
+});
+
+images[1].addEventListener("mouseout", function () {
+    images[1].src = "./assets/imgs/mask-group-1@2x.png";
+});
+
+images[2].addEventListener("mouseout", function () {
+    images[2].src = "./assets/imgs/mask-group-5@2x.png";
+});
+
+images[6].addEventListener("mouseout", function () {
+    images[6].src = "./assets/imgs/mask-group-8@2x.png";
+});
+
+images[5].addEventListener("mouseout", function () {
+    images[5].src = "./assets/imgs/mask-group-10@2x.png";
+});
+
+images[4].addEventListener("mouseout", function () {
+    images[4].src = "./assets/imgs/Frame 1000007304 (1).png";
+});
+
+images[3].addEventListener("mouseout", function () {
+    images[3].src = "./assets/imgs/mask-group-15@2x.png";
+});
+
+images[7].addEventListener("mouseout", function () {
+    images[7].src = "./assets/imgs/mask-group-17@2x.png";
+});
+
+images[8].addEventListener("mouseout", function () {
+    images[8].src = "./assets/imgs/Frame 1000007305.svg";
+});
+
+images[9].addEventListener("mouseout", function () {
+    images[9].src = "./assets/imgs/mask-group-18@2x.png";
+});
+
+
+
+
+
+
+images_desktop[0].addEventListener("mouseover", function () {
+    images_desktop[0].src = "./assets/imgs/image 1.png";
+});
+
+images_desktop[1].addEventListener("mouseover", function () {
+    images_desktop[1].src = "./assets/imgs/image 1.png";
+});
+
+images_desktop[2].addEventListener("mouseover", function () {
+    images_desktop[2].src = "./assets/imgs/image 1.png";
+});
+
+images_desktop[6].addEventListener("mouseover", function () {
+    images_desktop[6].src = "./assets/imgs/image 1.png";
+});
+
+images_desktop[5].addEventListener("mouseover", function () {
+    images_desktop[5].src = "./assets/imgs/image 1.png";
+});
+
+images_desktop[4].addEventListener("mouseover", function () {
+    images_desktop[4].src = "./assets/imgs/image 1.png";
+});
+
+images_desktop[3].addEventListener("mouseover", function () {
+    images_desktop[3].src = "./assets/imgs/image 1.png";
+});
+
+images_desktop[7].addEventListener("mouseover", function () {
+    images_desktop[7].src = "./assets/imgs/image 1.png";
+});
+
+images_desktop[8].addEventListener("mouseover", function () {
+    images_desktop[8].src = "./assets/imgs/image 1.png";
+});
+
+images_desktop[9].addEventListener("mouseover", function () {
+    images_desktop[9].src = "./assets/imgs/image 1.png";
+});
+
+images_desktop[0].addEventListener("mouseout", function () {
+    images_desktop[0].src = "./assets/imgs/mask-group@2x.png";
+});
+
+images_desktop[1].addEventListener("mouseout", function () {
+    images_desktop[1].src = "./assets/imgs/mask-group-1@2x.png";
+});
+
+images_desktop[2].addEventListener("mouseout", function () {
+    images_desktop[2].src = "./assets/imgs/mask-group-5@2x.png";
+});
+
+images_desktop[6].addEventListener("mouseout", function () {
+    images_desktop[6].src = "./assets/imgs/mask-group-8@2x.png";
+});
+
+images_desktop[5].addEventListener("mouseout", function () {
+    images_desktop[5].src = "./assets/imgs/mask-group-10@2x.png";
+});
+
+images_desktop[4].addEventListener("mouseout", function () {
+    images_desktop[4].src = "./assets/imgs/Frame 1000007304 (1).png";
+});
+
+images_desktop[3].addEventListener("mouseout", function () {
+    images_desktop[3].src = "./assets/imgs/mask-group-15@2x.png";
+});
+
+images_desktop[7].addEventListener("mouseout", function () {
+    images_desktop[7].src = "./assets/imgs/mask-group-17@2x.png";
+});
+
+images_desktop[8].addEventListener("mouseout", function () {
+    images_desktop[8].src = "./assets/imgs/Frame 1000007305.svg";
+});
+
+images_desktop[9].addEventListener("mouseout", function () {
+    images_desktop[9].src = "./assets/imgs/mask-group-18@2x.png";
+});
+
+
+
+images_table[0].addEventListener("mouseover", function () {
+    images_table[0].src = "./assets/imgs/image 1.png";
+});
+
+images_table[1].addEventListener("mouseover", function () {
+    images_table[1].src = "./assets/imgs/image 1.png";
+});
+
+images_table[2].addEventListener("mouseover", function () {
+    images_table[2].src = "./assets/imgs/image 1.png";
+});
+
+images_table[6].addEventListener("mouseover", function () {
+    images_table[6].src = "./assets/imgs/image 1.png";
+});
+
+images_table[5].addEventListener("mouseover", function () {
+    images_table[5].src = "./assets/imgs/image 1.png";
+});
+
+images_table[4].addEventListener("mouseover", function () {
+    images_table[4].src = "./assets/imgs/image 1.png";
+});
+
+images_table[3].addEventListener("mouseover", function () {
+    images_table[3].src = "./assets/imgs/image 1.png";
+});
+
+images_table[7].addEventListener("mouseover", function () {
+    images_table[7].src = "./assets/imgs/image 1.png";
+});
+
+images_table[8].addEventListener("mouseover", function () {
+    images_table[8].src = "./assets/imgs/image 1.png";
+});
+
+images_table[9].addEventListener("mouseover", function () {
+    images_table[9].src = "./assets/imgs/image 1.png";
+});
+
+images_table[0].addEventListener("mouseout", function () {
+    images_table[0].src = "./assets/imgs/mask-group@2x.png";
+});
+
+images_table[1].addEventListener("mouseout", function () {
+    images_table[1].src = "./assets/imgs/mask-group-1@2x.png";
+});
+
+images_table[2].addEventListener("mouseout", function () {
+    images_table[2].src = "./assets/imgs/mask-group-5@2x.png";
+});
+
+images_table[6].addEventListener("mouseout", function () {
+    images_table[6].src = "./assets/imgs/mask-group-8@2x.png";
+});
+
+images_table[5].addEventListener("mouseout", function () {
+    images_table[5].src = "./assets/imgs/mask-group-10@2x.png";
+});
+
+images_table[4].addEventListener("mouseout", function () {
+    images_table[4].src = "./assets/imgs/Frame 1000007304 (1).png";
+});
+
+images_table[3].addEventListener("mouseout", function () {
+    images_table[3].src = "./assets/imgs/mask-group-15@2x.png";
+});
+
+images_table[7].addEventListener("mouseout", function () {
+    images_table[7].src = "./assets/imgs/mask-group-17@2x.png";
+});
+
+images_table[8].addEventListener("mouseout", function () {
+    images_table[8].src = "./assets/imgs/Frame 1000007305.svg";
+});
+
+images_table[9].addEventListener("mouseout", function () {
+    images_table[9].src = "./assets/imgs/mask-group-18@2x.png";
+});
+
